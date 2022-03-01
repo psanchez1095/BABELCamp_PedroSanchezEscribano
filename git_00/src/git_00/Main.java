7e1ca1bc5b0434ae2bfa801bfb2fa59836c6fde4
Pedro Sanchez Escribano <pedro.sanchez@babelgroup.com>
2022-03-01 22:21:39 +0100

    Resta Operations Added in Main

--- git_00/src/git_00/Main.java
+++ git_00/src/git_00/Main.java
@@ -7,15 +7,20 @@ public class Main {
 		public static void main(String[] args) {
 
 			Suma s = new Suma();
+			Resta r = new Resta();
 			Scanner scan=new Scanner(System.in);
-			System.out.print("Ingresa dos numeros para la suma y la suma acumulada:\n");
+			System.out.print("Ingresa dos numeros para realizar las operaciones de suma y resta:\n");
 			
 			int a=Integer.parseInt(scan.next());
 			int b=Integer.parseInt(scan.next());
 			
 			System.out.print("La suma es "+s.suma(a, b)+"\n");
 			System.out.print("La suma acumlada es " +s.doAcum(a)+"\n");
-			System.out.print("La suma acumlada es " +s.doAcum(b)+"\n");
+			System.out.print("La suma acumlada es " +s.doAcum(b)+"\n\n");
+			
+			System.out.print("La resta es "+r.resta(a, b)+"\n");
+			System.out.print("La resta acumlada es " +r.doAcum(a)+"\n");
+			System.out.print("La resta acumlada es " +r.doAcum(b)+"\n");
 				
 		}
 
