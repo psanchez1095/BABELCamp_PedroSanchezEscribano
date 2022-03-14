61df190e5693fc7aa520796f776fd51801b0cc50
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-03-14 14:42:34 +0100

    Proyecto Web JS 05 -> JS CSS HTML Modified

--- 05_PROYECTO_WEB_JS/js/index.js
+++ 05_PROYECTO_WEB_JS/js/index.js
@@ -1,46 +1,42 @@
 //alert("Bienvenido al repositorio Ws_Javascript");
 console.log("Probando la consola")
 
-let arrayNumbers = []//array vacio
+let arrayNumbers = [] //array vacio
 
-function validatePush(){
+function validatePush() {
 
     let n = document.getElementById("num").value;
 
-    if(!isNaN(n) && n != "") {
+    if (!isNaN(n) && n != "") {
         arrayNumbers.push(n);
-        document.getElementById("num").value=""
-    
+        document.getElementById("num").value = ""
+    } else {
+        document.getElementById("textError").innerHTML = "Error. Debe añadir números"
+        document.getElementById("error").style.visibility = "visible"
+        setTimeout(error, 2000);
     }
-    else{
-        document.getElementById("textError").innerHTML ="Error. Debe añadir números"
-        document.getElementById("error").style.visibility="visible";
-        setTimeout( error, 2000);
-    }
-   
+
 }
 
-function extractRandom(){
+function extractRandom() {
 
-    if(arrayNumbers.length != 0){
+    if (arrayNumbers.length != 0) {
+        let indexRandom = getRandomInt(0, arrayNumbers.length)
+        document.getElementById("numExtract").value = arrayNumbers[indexRandom]
 
-    let indexRandom = getRandomInt(0,arrayNumbers.length);
-    document.getElementById("numExtract").value = arrayNumbers[indexRandom];
-
-    }
-    else {
-        document.getElementById("textError").innerHTML ="Error. El array se encuentra vacio"
-        document.getElementById("error").style.visibility="visible";
-        setTimeout( error, 2000);
+    } else {
+        document.getElementById("textError").innerHTML = "Error. El array se encuentra vacio"
+        document.getElementById("error").style.visibility = "visible"
+        setTimeout(error, 2000)
     }
 }
 
 function error() {
-    document.getElementById("error").style.visibility="hidden";
-  }
+    document.getElementById("error").style.visibility = "hidden"
+}
 
 
-  // Retorna un número aleatorio entre min (incluido) y max (excluido)
+// Retorna un número entero aleatorio entre min (incluido) y max (excluido)
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-  }
+}
