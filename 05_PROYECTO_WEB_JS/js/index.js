1cbff4d0f4903d74b8e8827dc99bcc40e9e15a82
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-03-14 14:10:03 +0100

    Proyecto Web JS 05 -> JS and HTML Modified

--- 05_PROYECTO_WEB_JS/js/index.js
+++ 05_PROYECTO_WEB_JS/js/index.js
@@ -8,15 +8,39 @@ function validatePush(){
     let n = document.getElementById("num").value;
 
     if(!isNaN(n) && n != "") {
-        arrayNumbers.push()
+        arrayNumbers.push(n);
+        document.getElementById("num").value=""
+    
     }
     else{
+        document.getElementById("textError").innerHTML ="Error. Debe añadir números"
         document.getElementById("error").style.visibility="visible";
         setTimeout( error, 2000);
     }
    
 }
 
+function extractRandom(){
+
+    if(arrayNumbers.length != 0){
+
+    let indexRandom = getRandomInt(0,arrayNumbers.length);
+    document.getElementById("numExtract").value = arrayNumbers[indexRandom];
+
+    }
+    else {
+        document.getElementById("textError").innerHTML ="Error. El array se encuentra vacio"
+        document.getElementById("error").style.visibility="visible";
+        setTimeout( error, 2000);
+    }
+}
+
 function error() {
     document.getElementById("error").style.visibility="hidden";
+  }
+
+
+  // Retorna un número aleatorio entre min (incluido) y max (excluido)
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
