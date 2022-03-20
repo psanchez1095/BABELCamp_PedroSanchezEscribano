27e2083d80ba4b05f49f733a9fa317c8f14dd2cc
SansanDevelop <pedros03@ucm.es>
2022-03-20 19:27:20 +0100

    Proyecto Web Angular 10 Form -> Fixing .ts and fixing responsive css

--- 10_PROYECTO_WEB_ANGULAR_FORM/src/app/main/main.component.ts
+++ 10_PROYECTO_WEB_ANGULAR_FORM/src/app/main/main.component.ts
@@ -33,32 +33,27 @@ export class MainComponent {
   deleteButtonDisabled: boolean = true;
   editButtonDisabled: boolean = true;
   clearButtonDisabled: boolean = true;
-  
+
   constructor() {
 
     //Para Pruebas
-    let game = new Game("1w"," 2", 63, 3)
+    /*let game = new Game("1w"," 2", 63, 3)
     let game2 = new Game("44r41"," 2ew", 43, 2)
     let game3 = new Game("ewr1"," 2ewr", 23, 4)
     let game4 = new Game("1qew"," 2ewrew", 13, 1)
     let game5 = new Game("erew1"," 2", 23, 55)
-    this.games.push(game,game2,game3,game4,game5)
+    this.games.push(game,game2,game3,game4,game5)*/
   }
 
-  public getGames() : Array<Game>{
+  public getGames(): Array<Game> {
     return this.games;
   }
 
   //Añade un juego al array con los datos del formulario
   public addGame() {
     //Compruebo que los campos no esten vacios y que la valoracion media es un número
-    if (
-      this.id != '' &&
-      this.titulo != '' &&
-      this.compania != '' &&
-      this.vMedia != '' &&
-      !isNaN(parseInt(this.vMedia))
-    ) {
+    if (this.id != '' && !isNaN(parseInt(this.id)) && this.titulo != '' &&this.compania != '' && this.vMedia != '' &&!isNaN(parseInt(this.vMedia))) {
+      
       //compruebo si existe el id en el array ayudandome de un Map
       if (this.map.has(this.id)) {
         this.errorMessage = 'Ya existe un juego con ese id.';
@@ -86,27 +81,28 @@ export class MainComponent {
     }
     //En caso de que algun campo este vacio y/o la valoración media sea un string
     else {
-      if (this.vMedia != '' && isNaN(parseInt(this.vMedia)))
-        this.errorMessage = 'La valoración media debe ser un número.';
-      else
-        this.errorMessage =
-          'Los campos titulo,compañia y valoración media no pueden estar vacios.';
+
+      if (this.id != '' && isNaN(parseInt(this.id))) this.errorMessage = 'El Id debe ser un número.';
+      else if (this.vMedia != '' && isNaN(parseInt(this.vMedia))) this.errorMessage = 'La valoración media debe ser un número.';
+      else this.errorMessage = 'Los campos titulo,compañia y valoración media no pueden estar vacios.';
+
       this.error = false;
 
       setInterval(() => {
         this.error = true;
-      }, 2500);
+      }, 3500);
+
     }
   }
-  
+
   //Borra un juego del array de juegos
   public delete() {
-    
+
   }
-  
+
   //Limpia los valores de un juego
   public edit() {
-    
+
   }
 
   //Limpia los campos del formulario
@@ -116,18 +112,19 @@ export class MainComponent {
     this.compania = '';
     this.vMedia = '';
   }
-  
+
   //Funcion que ese ejecuta cuando se selecciona un juego
-  public select(game : Game) {
-    
+  public select(game: Game) {
+
     this.id = game.getId().toString();
     this.titulo = game.getTitulo()
     this.compania = game.getCompania()
     this.vMedia = game.getValoracionMedia().toString()
-  
+
     this.addButtonDisabled = true
     this.editButtonDisabled = false
     this.deleteButtonDisabled = false
+
   }
-  
+
 }
